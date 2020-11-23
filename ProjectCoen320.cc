ece6c46cdb45c21cf52555aebe63f4059dd61c8f
ZackOrs <zachary_orsoli@hotmail.com>
2020-11-22 21:51:36 -0500

    First Commit

---
+++ ProjectCoen320.cc
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int myClock = 1;
+using namespace std;
+
+vector<string> split(string str){
+
+	vector<string> array;
+	stringstream ss(str);
+	string temp;
+
+	  while(getline(ss, temp, ',')) {
+	    array.push_back(temp);
+	  }
+	  return array;
+}
+
+
+string GetExcelData(int row, int column){
+	ifstream csvFile("/root/DrivingData.csv");
+	string lineText;
+	string cellData = "NOT GOOD";
+	if(csvFile){
+
+		while(getline(csvFile,lineText) && (row > 0)){
+			row--;
+		}
+
+		if(row == 0){
+			vector<string> array = split(lineText);
+			cellData = array[column];
+		}
+	}
+
+	return cellData;
+}
+
+void*  FuelConsumptionThread( void*  arg )
+{
+	int excelColumn = 0;
+	string fieldValue;
+
+	while(true){
+
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Fuel Consumption: "<< fieldValue << endl;
+	   usleep(10000);
+	}
+
+   return( 0 );
+}
+
+void*  RPMThread( void*  arg )
+{
+	int excelColumn = 12;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... RPM: "<< fieldValue << endl;
+		usleep(500000);
+	}
+
+   return( 0 );
+}
+
+void*  CoolantTempThread( void*  arg )
+{
+	int excelColumn = 17;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Coolant Temp: "<< fieldValue << endl;
+		   usleep(2000000);
+	}
+
+   return( 0 );
+}
+
+void*  GearThread( void*  arg )
+{
+	int excelColumn = 33;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Current Gear: "<< fieldValue << endl;
+		   usleep(100000);
+	}
+
+   return( 0 );
+}
+
+void*  TransmissionOilTempThread( void*  arg )
+{
+	int excelColumn = 34;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Transmission Oil Temp: "<< fieldValue << endl;
+		   usleep(5000000);
+	}
+
+   return( 0 );
+}
+
+void*  SpeedThread( void*  arg )
+{
+	int excelColumn = 43;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Current Speed: "<< fieldValue << endl;
+		   usleep(100000);
+	}
+
+   return( 0 );
+}
+
+void*  AccelerationThread( void*  arg )
+{
+	int excelColumn = 44;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Acceleration: "<< fieldValue << endl;
+		   usleep(150000);
+	}
+
+   return( 0 );
+}
+
+void*  BreakSwitchThread( void*  arg )
+{
+	int excelColumn = 45;
+	string fieldValue;
+	while(true){
+		fieldValue = GetExcelData(myClock,excelColumn);
+		cout << "CURRENT TIME:" << myClock << "...... Break Switch: "<< fieldValue << endl;
+		   usleep(100000);
+	}
+
+   return( 0 );
+}
+
+int main( void )
+{
+
+
+   pthread_attr_t attr;
+
+   pthread_attr_init( &attr );
+   pthread_attr_setdetachstate(
+      &attr, PTHREAD_CREATE_DETACHED );
+
+   pthread_t joinThread;
+   pthread_create( &joinThread, &attr, &RPMThread, NULL );
+   pthread_create( NULL, &attr, &CoolantTempThread, NULL );
+   pthread_create( NULL, &attr, &GearThread, NULL );
+   pthread_create( NULL, &attr, &TransmissionOilTempThread, NULL );
+   pthread_create( NULL, &attr, &SpeedThread, NULL );
+   pthread_create( NULL, &attr, &AccelerationThread, NULL );
+   pthread_create( NULL, &attr, &BreakSwitchThread, NULL );
+
+   printf( "Clock Starting \n");
+   /* Allow threads to run. */
+   while(true){
+//	   printf( "Clock time: %d \n", myClock );
+	   usleep(1000000);
+	   myClock++;
+   }
+   pthread_join(joinThread, NULL);
+
+
+   return EXIT_SUCCESS;
+}
+
